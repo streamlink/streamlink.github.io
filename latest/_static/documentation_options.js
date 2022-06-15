da481d18201c5105f5d9d159c8aae12f8cb03008
streamlinkbot <streamlinkbot@users.noreply.github.com>
2022-06-15 07:34:42 +0000

    Update docs from Github build 3131
    of streamlink/streamlink
    
    The docs were built from the branch 'master' against the commit
    361f8971c30a6d397784084e1968c7921da1d131
    
    https://github.com/streamlink/streamlink/commit/361f897
    https://github.com/streamlink/streamlink/actions/runs/2500524344

--- latest/_static/documentation_options.js
+++ latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '4.1.0+23.gcfb482ca',
+    VERSION: '4.1.0+25.g361f8971',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
