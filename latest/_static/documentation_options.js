bfb5e5868245e839094e3ca0776de82c63a9954d
streamlinkbot <streamlinkbot@users.noreply.github.com>
2023-05-24 16:45:29 +0000

    Update docs from Github build 4475
    of streamlink/streamlink
    
    The docs were built from the branch 'master' against the commit
    745791cf21ba7ed4c15b7872f85d9c61b44dd5dc
    
    https://github.com/streamlink/streamlink/commit/745791c
    https://github.com/streamlink/streamlink/actions/runs/5071247011

--- latest/_static/documentation_options.js
+++ latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '5.5.1+14.ge03bb5b4',
+    VERSION: '5.5.1+15.g745791cf',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
