c2c8fc7bdaf8cf66a1ce745adae8111371a3a915
streamlinkbot <streamlinkbot@users.noreply.github.com>
2023-06-22 21:20:53 +0000

    Update docs from Github build 4563
    of streamlink/streamlink
    
    The docs were built from the branch 'master' against the commit
    5ef0280ab0e7c4f4bdc281058625e1ec85f8c3f5
    
    https://github.com/streamlink/streamlink/commit/5ef0280
    https://github.com/streamlink/streamlink/actions/runs/5350406864

--- latest/_static/documentation_options.js
+++ latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '5.5.1+50.g9c6dd568',
+    VERSION: '5.5.1+51.g5ef0280a',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
